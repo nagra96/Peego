23100637bb68bc1d7074a8f8ab6abc1bfc2f65a6
nagra <god>
2025-09-13 16:20:11 -0600

    Initial Commit

---
+++ to/toApp.swift
@@ -0,0 +1,17 @@
+//
+//  toApp.swift
+//  to
+//
+//  Created by RAJVIR KAUR on 2025-09-13.
+//
+
+import SwiftUI
+
+@main
+struct toApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
